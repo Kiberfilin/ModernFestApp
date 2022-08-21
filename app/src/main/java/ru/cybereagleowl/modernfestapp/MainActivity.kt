cd2a5dd7a87ddbe384f679ab58c8db62abb3cd5a
Kiberfilin <kiberfilin@gmail.com>
2022-08-21 19:14:30 +0300

    vk.com android sdk integration checked ok

--- app/src/main/java/ru/cybereagleowl/modernfestapp/MainActivity.kt
+++ app/src/main/java/ru/cybereagleowl/modernfestapp/MainActivity.kt
@@ -10,12 +10,16 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAuthenticationResult
+import com.vk.api.sdk.auth.VKScope
 import ru.cybereagleowl.modernfestapp.ui.theme.ModernFestAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        authLauncher.launch(arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+        /*setContent {
             ModernFestAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -23,6 +27,29 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+                }
+            }
+        }*/
+    }
+
+    private val authLauncher = VK.login(this) { result: VKAuthenticationResult ->
+        setContent {
+            ModernFestAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    when (result) {
+                        is VKAuthenticationResult.Success -> {
+                            // User passed authorization
+                            Greeting("User passed authorization")
+                        }
+                        is VKAuthenticationResult.Failed -> {
+                            // User didn't pass authorization
+                            Greeting("User didn't pass authorization")
+                        }
+                    }
                 }
             }
         }
